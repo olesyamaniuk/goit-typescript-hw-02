1f97803c263a69aa4d17d66010eaa90011d155a4
olesyamaniuk <maniukolesya222@gmail.com>
2024-05-30 17:21:17 +0300

    4

--- src/components/App/App.types.ts
+++ src/components/App/App.types.ts
@@ -1,32 +1,8 @@
-// export interface Image {
-//     id: string;
-//     title: string;
-//     urls: {
-//       small: string;
-//       regular: string;
-//     };
-//     alt_description: string;
-//   }
-// export interface Image {
-//   id: string;
-//   description: string | null;
-//   alt_description: string;
-//   urls: {
-//     raw: string;
-//     full: string;
-//     regular: string;
-//     small: string;
-//     thumb: string;
-//   };
-//   user: {
-//     name: string;
-//   };
-// }
 
 export interface Image {
   id: string;
   description: string | null;
-  alt_description: string; // Change type from string | null to string
+  alt_description: string;
   urls: {
     raw: string;
     full: string;
--- src/components/showImage.ts
+++ src/components/showImage.ts
@@ -1,40 +1,9 @@
-// import axios from "axios";
-// axios.defaults.baseURL = "https://api.unsplash.com/";
 
-// const MyAccess = "AIK1kkcDQtr5tx71hXjaprpcLRmG884OQKAMFhDVvag";
-
-// export const fetchImages = async (searchQuery: string, currenPage: number) => {
-//   const response = await axios.get(`search/photos/?client_id=${MyAccess}`, {
-//     params: {
-//       query: searchQuery,
-//       page: currenPage,
-//       per_page: 12,
-//     },
-//   });
-//   console.log(response.data.results);
-//   return response.data.results;
-// };
 import axios from "axios";
-
 axios.defaults.baseURL = "https://api.unsplash.com/";
 
 const MyAccess = "AIK1kkcDQtr5tx71hXjaprpcLRmG884OQKAMFhDVvag";
 
-// interface Image {
-//   id: string;
-//   description: string | null;
-//   alt_description: string;
-//   urls: {
-//     raw: string;
-//     full: string;
-//     regular: string;
-//     small: string;
-//     thumb: string;
-//   };
-//   user: {
-//     name: string;
-//   };
-// }
 
 export const fetchImages = async (searchQuery: string, currentPage: number) => {
   try {
@@ -46,7 +15,7 @@ export const fetchImages = async (searchQuery: string, currentPage: number) => {
         per_page: 12,
       },
     });
-    return response.data.results; // Ensure this returns an array of images
+    return response.data.results;
   } catch (error) {
     console.error("Error fetching images:", error);
     throw error;
